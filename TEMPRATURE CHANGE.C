193c95eedd84cfc6b371568464f3bfde77fce5f6
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-03 18:25:32 +0530

    Add files via upload

---
+++ TEMPRATURE CHANGE.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("Enter the temprature in F =");
+    scanf("%f",&F);
+    C=5*C/9-32;
+    printf("The temprature in C =%f",C);
+    return 0;
+}
