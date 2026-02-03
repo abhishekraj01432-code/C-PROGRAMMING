2a8fe5bf9748b14aeb1d7ddb1b33d018b3363d2f
ABHISHEK VERMA <abhishekraj01432@gmail.com>
2026-02-03 18:24:17 +0530

    Add files via upload

---
+++ SQUARE AND CUBE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,S,C;
+    printf("Enter the value of a =");
+    scanf("%d",&a);
+    S = a*a;
+    C = S*a;
+    printf("The Square is =%d",S);
+    printf("The Cube is =%d",C);
+    return 0;
+}
